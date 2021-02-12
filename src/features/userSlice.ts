2ad0bd873987f8b671047ceb38f4d7f6f2d72402
Ammar <ammarraneez@gmail.com>
2021-02-12 23:58:24 +0530

    starting off w redux user slice

---
+++ src/features/userSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../app/store';
+
+interface userState {
+    user: string;
+}
+
+const initialState: userState = {
+    user: "",
+};
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        login: (state, action: PayloadAction<string>) => {
+            state.user = action.payload;
+        },
+        logout: state => {
+            state.user = "";
+        }
+    },
+});
+
+export const { login, logout } = userSlice.actions;
+export const selectUser = (state: RootState) => state.user.user;
+export default userSlice.reducer;
