c369be07690e352f8feaf1531fc93af2c07a0c71
Ammar <ammarraneez@gmail.com>
2021-02-12 22:28:24 +0530

    starting on main chat section

--- src/features/Chat/Chat.tsx
+++ src/features/Chat/Chat.tsx
@@ -4,7 +4,10 @@ import styles from './Chat.module.css'
 export function Chat() {
     return (
         <div className={styles.chat}>
-            
+            <div className={styles.chat__header}>
+                <h4>To: <span className={styles.chat__name}>Channel name</span></h4>
+                <strong>Details</strong>
+            </div>
         </div>
     )
 }
