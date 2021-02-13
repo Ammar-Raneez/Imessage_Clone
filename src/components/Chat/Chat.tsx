371e19788bbc1a55c855282ad54f9a728c226a95
Ammar <ammarraneez@gmail.com>
2021-02-14 00:45:48 +0530

    created Messages type and fetched message data

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,15 +1,35 @@
 import { IconButton } from '@material-ui/core';
 import MicNoneIcon  from '@material-ui/icons/MicNone';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
-import { selectchatName } from '../../features/chatSlice';
+import { selectchatId, selectchatName } from '../../features/chatSlice';
+import db from '../../firebase';
+import { IMessage } from '../../types';
 import { Message } from '../Message/Message';
 import { ChatWrapper } from './Chat.styles';
 
 export function Chat() {
     const chatName = useSelector(selectchatName);
+    const chatId = useSelector(selectchatId);
     const [input, setInput] = useState<string>("");
-    const [messages, setMessages] = useState([]);
+    const [messages, setMessages] = useState<IMessage[]>([]);
+
+    useEffect(() => {
+        if (chatId) {
+            db.collection('chats')
+                .doc(chatId)
+                .collection("messages")
+                .orderBy("timestamp", "desc")
+                .onSnapshot(snapshot => (
+                    setMessages(
+                        snapshot.docs.map(doc => ({
+                            id: doc.id,
+                            data: doc.data()
+                        }))
+                    )
+                ))
+        }
+    }, [])
 
     const sendMessage = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         event.preventDefault();
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,6 @@ export interface ISidebarChat {
     id: string,
     data: any
 }
+
+//gives all data to IMessage
+export interface IMessage extends ISidebarChat {};
